55787500c882d690c9b69f0efccc94974f09d591
no body know this <no.body.can.know.reason@gmail.com>
2021-06-25 00:31:55 +0530

    all thing isa dded

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/** @format */
+
+function closemenu() {
+	document.getElementById("navbar").style.left = "-280px";
+}
+function openmenu() {
+	document.getElementById("navbar").style.left = "0px";
+}
